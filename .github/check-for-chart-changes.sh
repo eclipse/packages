3198c3a7b2fb52853f77ce5b8fcfa1da207beceb
André Bauer <monotek23@gmail.com>
2019-12-16 21:33:37 +0100

    added license
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -3,6 +3,17 @@
 # check for chart changes to speedup ci
 #
 
+# Copyright (c) 2019 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+
 set -o errexit
 set -o pipefail
 
