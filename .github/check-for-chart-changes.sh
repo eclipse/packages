31ed66223137acac38fbdaa91091470bec48f472
Axel Köhler <axel.koehler@kiwigrid.com>
2020-01-09 08:11:08 +0100

    Do not fail, when there are no chart changes
    Signed-off-by: Axel Köhler <axel.koehler@kiwigrid.com>

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -25,8 +25,8 @@ git remote add chart-changes "${CHART_REPO}"
 git fetch chart-changes master
 
 if [ -z "$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/chart-changes/master -- charts)" ]; then
-  echo -e "\n\n Error! No chart changes detected! Exiting... \n"
-  exit 1
+  echo -e "\n\n No chart changes detected! Exiting... \n"
+  exit 0
 else
   echo -e "\n Changes found... Continue with next job... \n"
 fi
