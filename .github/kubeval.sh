48c62f7c093bd007070c058c1bb388f22e904388
Kai Hudalla <kai.hudalla@bosch.io>
2021-05-20 09:01:55 +0200

    fixup
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -3,7 +3,7 @@
 # use kubeval to validate helm generated kubernetes manifest
 #
 
-# Copyright (c) 2019, 2020 Contributors to the Eclipse Foundation
+# Copyright (c) 2019, 2021 Contributors to the Eclipse Foundation
 #
 # See the NOTICE file(s) distributed with this work for additional
 # information regarding copyright ownership.
@@ -18,14 +18,14 @@ set -o errexit
 set -o pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts packages | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-HELM_VERSION="v3.1.2"
-KUBEVAL_VERSION="0.15.0"
+#HELM_VERSION="v3.1.2"
+KUBEVAL_VERSION="v0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install helm
-curl --silent --show-error --fail --location --output get_helm.sh https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get
-chmod 700 get_helm.sh
-./get_helm.sh --version "${HELM_VERSION}"
+#curl --silent --show-error --fail --location --output get_helm.sh https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get
+#chmod 700 get_helm.sh
+#./get_helm.sh --version "${HELM_VERSION}"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
@@ -46,3 +46,4 @@ for CHART_DIR in ${CHART_DIRS};do
   echo "kubeval(idating) ${CHART_DIR##charts/} chart..."
   helm template "${CHART_DIR}" | kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
+
