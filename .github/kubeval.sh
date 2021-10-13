f825ce9619eaa1a0772c3991c697e1b79ea96240
Jens Reimann <jreimann@redhat.com>
2021-10-13 17:10:14 +0200

    Allow charts to customize CI values
    This allows charts to create a customized values
    file for the chart testing, using e.g. a Kind ingress
    IP.

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -33,7 +33,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 
 # validate charts
-for CHART_DIR in ${CHART_DIRS};do
+for CHART_DIR in ${CHART_DIRS}; do
   echo "helm dependency build..."
   helm dependency build "${CHART_DIR}"
 
