b0430afbfe649b31b1796bf9dc2cab64f0479d6d
Kai Hudalla <kai.hudalla@bosch.io>
2022-02-16 11:40:54 +0100

    [#345] Use Jolokia HTTP API for k8s probes
    The k8s readiness and liveness probes have been changed to no longer use
    a TCP connection attempt but to instead query the broker state via the
    Jolokia HTTP API.
    
    Fixes #345
    
    Signed-off-by: Kai Hudalla <kai.hudalla@bosch.io>

---
+++ charts/hono/config/artemis/liveness-probe.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# liveness-probe - checks if a given ActiveMQ Artemis broker instance is running
+#
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+##
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# liveness-probe [broker_name]
+#
+
+brokerName=${1:-hono-broker}
+url="http://localhost:8161/console/jolokia/read/org.apache.activemq.artemis:broker=%22${brokerName}%22/Version"
+
+curl -s --user artemis:artemis -H "Origin: http://localhost" ${url}
+
