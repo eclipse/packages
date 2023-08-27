f621f8a624eda5613aa5d983571d7e6e7ce19865
Carsten Lohmann <carsten.lohmann@bosch.io>
2023-08-27 15:22:30 +0200

    #488 Fix cloud2edge post-install hook.
    Signed-off-by: Carsten Lohmann <carsten.lohmann@bosch.io>

---
+++ packages/cloud2edge/post-install/post-install.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+#*******************************************************************************
+# Copyright (c) 2023 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
+DITTO_DEVOPS_USER_PW="devops:$(cat /var/run/c2e/ditto-gw-users/devops-password)"
+DEVICE_REGISTRY_BASE_URL="http://{{ .Release.Name }}-service-device-registry:8080/v1"
+# TODO use HTTP API instead of piggyback commands (https://eclipse.dev/ditto/connectivity-manage-connections.html)
+DITTO_CONNECTIVITY_URL="http://{{ .Release.Name }}-ditto-nginx:8080/devops/piggyback/connectivity"
+DITTO_THINGS_BASE_URL="http://{{ .Release.Name }}-ditto-nginx:8080/api/2/things"
+
+DEMO_TENANT="{{ .Values.demoDevice.tenant }}"
+DEMO_DEVICE="$DEMO_TENANT:{{ .Values.demoDevice.deviceId }}"
+IS_USING_AMQP="{{- if ( has "amqp" .Values.hono.messagingNetworkTypes ) }}true{{ else }}false{{ end }}"
+IS_USING_KAFKA="{{- if ( has "kafka" .Values.hono.messagingNetworkTypes ) }}true{{ else }}false{{ end }}"
+
+check_status() {
+  exit_code="$1"
+  response_body_and_status="$2"
+
+  if [ "$exit_code" -ne 0 ]; then
+    echo "curl command failed [exit-code: $exit_code]"
+    exit 1
+  fi
+  http_code=$(echo "$response_body_and_status" | tail -n1) # get last line
+  body=$(echo "$response_body_and_status" | sed '$ d') # get all but last line
+  # echo "DEBUG: Got [status: $http_code, response: $body]"
+  if [ "$http_code" -eq "409" ]; then
+    # HTTP 409 Conflict (e.g. "device already exists") ignored here
+    echo "Ignoring Http response 409 [$body]"
+  elif [ "$http_code" -ge "400" ]; then
+    echo "Http request failed [status: $http_code, response: $body]"
+    exit 1
+  fi
+}
+
+add_hono_tenant(){
+  tenant_id="$1"
+  http_request_body="$2"
+
+  echo "Adding tenant [$tenant_id]"
+  response_body_and_status=$(curl --silent --write-out "\n%{http_code}" \
+                    -X POST --header 'Content-Type: application/json' \
+                    --data-raw "$http_request_body" "$DEVICE_REGISTRY_BASE_URL/tenants/$tenant_id")
+  check_status $? "$response_body_and_status"
+}
+
+register_hono_device(){
+  tenant_id="$1"
+  device_id="$2"
+  http_request_body="$3"
+
+  echo "Registering device [tenant: $tenant_id, device: $device_id]"
+  response_body_and_status=$(curl --silent --write-out "\n%{http_code}" \
+                  -X POST --header 'Content-Type: application/json' \
+                  --data-raw "$http_request_body" "$DEVICE_REGISTRY_BASE_URL/devices/$tenant_id/$device_id")
+  check_status $? "$response_body_and_status"
+}
+
+add_hono_device_credentials(){
+  tenant_id="$1"
+  device_id="$2"
+  http_request_body_file="$3"
+
+  echo "Adding credentials [tenant: $tenant_id, device: $device_id]"
+  response_body_and_status=$(curl --silent --write-out "\n%{http_code}" \
+                -X PUT --header 'Content-Type: application/json' \
+                --data-binary "@$http_request_body_file" "$DEVICE_REGISTRY_BASE_URL/credentials/$tenant_id/$device_id")
+  check_status $? "$response_body_and_status"
+}
+
+add_connection_in_ditto(){
+  connection_name="$1"
+  http_request_body_file="$2"
+
+  echo "Adding $connection_name ditto connection [URL: $DITTO_CONNECTIVITY_URL]"
+  response_body_and_status=$(curl --silent --write-out "\n%{http_code}" \
+                -X POST -u "$DITTO_DEVOPS_USER_PW" --header 'Content-Type: application/json' \
+                --data-binary "@$http_request_body_file" "$DITTO_CONNECTIVITY_URL")
+  check_status $? "$response_body_and_status"
+}
+
+add_ditto_device(){
+  device="$1"
+  http_request_body_file="$2"
+  
+  echo "Adding '$device' ditto thing [URL: $DITTO_THINGS_BASE_URL/$device]"
+  response_body_and_status=$(curl --silent --write-out "\n%{http_code}" \
+                -X PUT -u ditto:ditto --header 'Content-Type: application/json' \
+                --data-binary "@$http_request_body_file" "$DITTO_THINGS_BASE_URL/$device")
+  check_status $? "$response_body_and_status"
+}
+
+# ----------------------------------------------
+
+echo "Device Registry Http endpoint base url: $DEVICE_REGISTRY_BASE_URL"
+add_hono_tenant "$DEMO_TENANT" "{}"
+register_hono_device "$DEMO_TENANT" "$DEMO_DEVICE" "{}"
+add_hono_device_credentials "$DEMO_TENANT" "$DEMO_DEVICE" "demo-device-credentials.json"
+
+if [ "$IS_USING_AMQP" = "true" ]; then
+  add_connection_in_ditto "Hono AMQP" "hono-amqp-connection.json"
+fi
+if [ "$IS_USING_KAFKA" = "true" ]; then
+  cp hono-kafka-connection.json /tmp/hono-kafka-connection.json
+  chmod 777 /tmp/hono-kafka-connection.json
+  certPath=/var/run/c2e/kafka-connection-cert/tls.crt
+  if [ -e $certPath ]; then
+      echo "Adapting hono-kafka-connection.json"
+      # Necessary transformation in order to comply with the expected format of certificates
+      # imposed by the Ditto connectivity API: "ca": "-----BEGIN CERTIFICATE-----\n<trusted certificate>\n-----END CERTIFICATE-----"
+      cert="$(< $certPath tr -d '\n' | sed 's/E-----/E-----\\\\n/g' | sed 's/-----END/\\\\n-----END/g')"
+      sed -i '/uri/ a '"\"ca\": \"$cert\","'' /tmp/hono-kafka-connection.json
+  fi
+  add_connection_in_ditto "Hono Kafka" "/tmp/hono-kafka-connection.json"
+fi
+
+add_ditto_device "$DEMO_DEVICE" "demo-device-thing.json"
+
+echo "DONE"
